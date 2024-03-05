3a22bbed73fadf1abfc43306947c56a6d08b164c
mousavi <mousavics2005@gmail.com>
2024-03-05 19:50:55 +0330

    creating classes

--- project.cpp
+++ project.cpp
@@ -92,15 +92,161 @@ private:
 public:
     strongZombie(int health, int damagePower);
     void attack(){
-        
+
     }
 };
-
 strongZombie::strongZombie(int health , int damagePower)
 {
     this->health = health;
     this->damagePower = damagePower;
 }
+
+
+class firearm
+{
+private:
+    string name;
+    int damage;
+    int price;
+
+    firearm() = default;
+public:
+    firearm(string name, int damage, int price);
+
+    void setName(string name){
+        this->name = name;
+    }
+    string getName(){
+        return name;
+    }
+
+    void setDamage(int damage){
+        this->damage = damage;
+    }
+    int getDamage(){
+        return damage;
+    }
+
+    void setPrice(int price){
+        this->price = price;
+    }
+    int getPrice(){
+        return price;
+    }
+
+};
+firearm::firearm(string name, int damage, int price)
+{
+    this->name = name;
+    this->damage = damage;
+    this->price = price;
+}
+
+
+class coldWeapon
+{
+private:
+    string name;
+    int damage;
+    int price;
+
+    coldWeapon() = default;
+public:
+    coldWeapon(string name, int damage, int price);
+
+    void setName(string name){
+        this->name = name;
+    }
+    string getName(){
+        return name;
+    }
+
+    void setDamage(int damage){
+        this->damage = damage;
+    }
+    int getDamage(){
+        return damage;
+    }
+
+    void setPrice(int price){
+        this->price = price;
+    }
+    int getPrice(){
+        return price;
+    }
+
+};
+coldWeapon::coldWeapon(string name, int damage, int price)
+{
+    this->name = name;
+    this->damage = damage;
+    this->price = price;
+}
+
+
+class throwableWeapon
+{
+private:
+    string name;
+    int damage;
+    int price;
+
+    throwableWeapon() = default;
+public:
+    throwableWeapon(string name, int damage, int price);
+
+    void setName(string name){
+        this->name = name;
+    }
+    string getName(){
+        return name;
+    }
+
+    void setDamage(int damage){
+        this->damage = damage;
+    }
+    int getDamage(){
+        return damage;
+    }
+
+    void setPrice(int price){
+        this->price = price;
+    }
+    int getPrice(){
+        return price;
+    }
+
+};
+throwableWeapon::throwableWeapon(string name, int damage, int price)
+{
+    this->name = name;
+    this->damage = damage;
+    this->price = price;
+}
+
+
+class shop
+{
+private:
+    //Firearms
+    firearm gun1;
+    firearm gun2;
+    firearm granade;
+    firearm granade2;
+    //Cold Weapons
+    coldWeapon knife;
+    coldWeapon knife2;
+    //Throwable
+
+    shop(/* args */);
+
+};
+
+// shop::shop(/* args */)
+// {
+// }
+
+
 
 
 
