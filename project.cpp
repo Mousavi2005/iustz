28d6f3953341db6831b9c47375d98329631df708
mousavi <mousavics2005@gmail.com>
2024-03-05 19:03:45 +0330

    creating classes

--- project.cpp
+++ project.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+class Human_character
+{
+private:
+    string name;
+    int age;
+public:
+    void setName(string name){
+        this->name = name;
+    }
+    Human_character(/* args */);
+    ~Human_character();
+};
+Human_character::Human_character(/* args */)
+{
+}
+
+Human_character::~Human_character()
+{
+}
+
+
 int main(){
     
 }
